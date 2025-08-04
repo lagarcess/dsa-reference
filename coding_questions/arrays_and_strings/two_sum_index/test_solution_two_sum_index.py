f888efb4a57bd49c48613097d6832a3465e4acc2
Lucas Garces <56943455+srgarcess@users.noreply.github.com>
2025-08-03 22:44:35 -0600

    Implement Two Number Sum Index solution and corresponding tests; add README for problem description

---
+++ coding_questions/arrays_and_strings/two_sum_index/test_solution_two_sum_index.py
@@ -0,0 +1,21 @@
+import pytest
+from .solutions_01 import twoSumIndex as twoSumIndex_01
+
+solutions_to_test = [
+    twoSumIndex_01,
+]
+
+test_cases = [
+    ([2, 7, 11, 15], 9, [0, 1]),
+    ([3, 2, 4], 6, [1, 2]),
+    ([3, 3], 6, [0, 1]),
+]
+
+@pytest.mark.parametrize("solution_func", solutions_to_test)
+@pytest.mark.parametrize(
+    "nums, target, expected",
+    test_cases
+)
+def test_two_sum_index(solution_func, nums, target, expected):
+    result = solution_func(nums, target)
+    assert result == expected
